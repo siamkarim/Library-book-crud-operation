2dbc6f6c96a42ca7329c14d5447b9a0bf148f59b
siamkarim <siamhossain518@gmail.com>
2022-10-12 22:10:36 +0600

    upload view

--- library/book/views.py
+++ library/book/views.py
@@ -8,3 +8,15 @@ from django.http import HttpResponse
 def index(request):
     shelf = Book.objects.all()
     return render(request, 'book/library.html', {'shelf': shelf})
+    
+def upload(request):
+    upload = BookCreate()
+    if request.method == 'POST':
+        upload = BookCreate(request.POST, request.FILES)
+        if upload.is_valid():
+            upload.save()
+            return redirect('index')
+        else:
+            return HttpResponse(""" Something went wrong. Please reload the webpage by clicking <a href="{{url:'index'}}>Reload</a>" """)
+    else:
+        return render(request, 'book/upload_form.html', {'upload_form': upload})
