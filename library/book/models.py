6e47cf348af99c2bff9fe19a4f688a9485d4454c
siamkarim <siamhossain518@gmail.com>
2022-10-12 12:27:03 +0600

    model  data create

--- library/book/models.py
+++ library/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField()
+    author = models.CharField(max_length=30, default='Guest')
+    email = models.EmailField(blank=True)
+    description = models.TextField(default='Available in IUBAT')
+
+    def __str__(self):
+        return self.name
+    
